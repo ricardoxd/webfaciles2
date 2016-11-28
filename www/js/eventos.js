6475e4cdc81e4a44d7a8fa6d9f145b4a53e8ce2f
ricardoxd <chessdj@hotmail.com>
2016-11-28 13:29:56 -0900

    commit from webfaciles2

--- www/js/eventos.js
+++ www/js/eventos.js
@@ -68,8 +68,11 @@ function popupCenter(url, w, h) {
   h=screen.height;
 var left = (screen.width/2)-(w/2);
 var top = (screen.height/2)-(h/2);
+if(window.opener==null){
+popup = window.open(url, "_blank", 'location=no');
+}else{
 popup = window.open(url, "Nuevo"+Math.random(), 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-
+}
 
 if(window.opener==null){
   popup.addEventListener( "loadstop", function() {
